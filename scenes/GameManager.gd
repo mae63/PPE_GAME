e0ad061f6f7417ccacc6e472a5f2a8afd0931145
crrenj <juanlantiguajorge@gmail.com>
2025-03-22 19:09:26 +0100

    Ajout de mon code perso

---
+++ scenes/GameManager.gd
@@ -0,0 +1,17 @@
+extends Node
+
+
+var is_hard_mode = false  # Mode normal par défaut
+
+func set_hard_mode(value: bool):
+	is_hard_mode = value
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
