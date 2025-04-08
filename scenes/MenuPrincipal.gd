924cb83f7d0d5827cfc456f4b52e7112be6e0298
Piriyatharsini <sureshpiriya9@gmail.com>
2025-04-08 14:55:38 +0200

    Menu modified

--- scenes/MenuPrincipal.gd
+++ scenes/MenuPrincipal.gd
@@ -5,17 +5,20 @@ extends Control
 @onready var quit_button = $TextureRect/QuitButton
 @onready var hard_mode_label = $HardModeLabel
 
+
 func _ready():
 	start_button.pressed.connect(_on_start_pressed)
 	hard_mode_button.pressed.connect(_on_hard_mode_pressed)
 	quit_button.pressed.connect(_on_quit_pressed)
+	
 
 func _on_start_pressed():
 	get_tree().change_scene_to_file("res://scenes/quiz.tscn")  # Charger la scène du quiz
 
 func _on_hard_mode_pressed():
 	GameManager.set_hard_mode(true)  # Activer le mode difficile
-	hard_mode_label.text = "Hard Mode: On"
+	hard_mode_label.text = "Hard Mode: ON"
+	
 
 func _on_quit_pressed():
 	get_tree().quit()  # Quitter le jeu
